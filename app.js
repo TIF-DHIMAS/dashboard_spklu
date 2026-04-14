1ef0f462b153a1b9680ac18e9c39a8237e5612f4
TIF-DHIMAS-MITRA <dhimas.wahyu88@gmail.com>
2026-04-14 23:09:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function renderTable(data) {
 
             const tr = document.createElement('tr');
             tr.innerHTML = `
-                <td><strong>${row['Nama Stasiun']}</strong></td>
+                <td><strong>${row['ID_SPKLU']}-${row['Nama Stasiun']}</strong></td>
                 <td>${row.RATA2TRANSAKSI} kali/bln</td>
                 <td>${Number(row.score).toFixed(4)}</td>
                 <td><span class="badge ${badgeClass}">${row.REKOMENDASI}</span></td>
