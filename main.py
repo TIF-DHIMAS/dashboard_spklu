e2be02b199d2bf41c6527bf9cc6dc1d1ad6cc695
TIF-DHIMAS-MITRA <dhimas.wahyu88@gmail.com>
2026-04-19 18:38:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ def main():
         df_m = df_m.set_index(df_m.columns[0])
 
         # bersihkan data (koma -> titik)
-        df_m = df_m.applymap(lambda x: str(x).strip().replace(',', '.'))
-        df_m = df_m.apply(pd.to_numeric, errors='coerce')
-        df_m = df_m.fillna(0)
-
+        df_m = df_m.astype(str)
+        for col in df_m.columns:
+        df_m[col] = df_m[col].str.strip().str.replace(',', '.', regex=False)
+        df_m = df_m.apply(pd.to_numeric, errors='coerce').fillna(0)
         matrix = df_m.values
 
         # hitung bobot AHP
